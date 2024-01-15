cdba58004d2b5b1935c0a86827666fff80cc2392
AdedamolaXL <adedamolaxl@outlook.com>
2024-01-15 12:37:47 +0100

    add components

---
+++ frontend/src/components/player.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useAsset } from "@livepeer/react";
+import Plyr, { PlyrInstance } from "plyr-react";
+import "plyr-react/plyr.css";
+
+export default function Player({ id }) {
+    
+    const { data: asset } = useAsset(id);
+
+    return (
+        <Plyr
+            source={{
+                type: "video",
+                title: asset?.name,
+
+                sources: [
+                    {
+                        src: asset?.downloadUrl,
+                        type: "video/mp4",
+                    },
+                ],
+            }}
+            options={{
+                autoplay: true,
+            }}
+                autoplay={true}
+            />
+    );
+}
